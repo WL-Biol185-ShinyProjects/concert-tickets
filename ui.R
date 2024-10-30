0664101c86ea723ab510676628e9c7f2b5e857dc
jcluzzio <luzzioj25@mail.wlu.edu>
2024-10-30 14:44:32 -0400

    Began our website (Title and NavBar)

---
+++ ui.R
@@ -0,0 +1,7 @@
+library(shiny)
+titlePanel("Concert Tickets USA")
+navbarPage("Concert Tickets USA",
+           tabPanel("Welcome"),
+           tabPanel("Raw Data"),
+           tabPanel("Maps"),
+           tabPanel("Graphical Cost Comparisons"))
