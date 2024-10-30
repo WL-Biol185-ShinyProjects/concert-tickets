a514636518510027e346dc387966eb63b4048824
jcluzzio <luzzioj25@mail.wlu.edu>
2024-10-30 16:12:56 -0400

    Added theme, titles, describing text, and added panels

--- ui.R
+++ ui.R
@@ -1,7 +1,17 @@
 library(shiny)
-titlePanel("Concert Tickets USA")
-navbarPage("Concert Tickets USA",
-           tabPanel("Welcome"),
-           tabPanel("Raw Data"),
-           tabPanel("Maps"),
-           tabPanel("Graphical Cost Comparisons"))
+library(shinythemes)
+navbarPage(theme = shinytheme("flatly"),
+  "Concert Tickets USA",
+           tabPanel("Welcome",
+                    h1 ("Welcome to Concert Tickets USA"),
+                    h3("Concert Tickets USA is your virtual guide to concert ticket understanding. Our site displays connections between ticket prices, artistis, locations, venues, and more!"),
+                    h4("About the Creators:"),
+                    HTML("We are four college students at Washington and Lee University in Lexington Virginia. For full time students, concerts can be a fun night out after and exhausting week. 
+                         However, concerts are pricey. Concert Tickets USA allows us to predict ticket prices based on a multitude of factors, and financially plan for that cost.")),
+           tabPanel("Maps",
+                    h1("Ticket Price, Artist, and Venue Map Data")),
+           tabPanel("Graphical Cost Comparisons",
+                    h1("Concert Ticket Graphical Cost Comparisons"),
+                    HTML("City by City Cost Context and Ticket Vendor Comparisons")),
+           tabPanel("Raw Data",
+                    h1("Raw Datasets for Ticket Data and Cost of Living")))
