43423a9cc28f6b9a1656b81c72cbb5823dfd9bd4
jcluzzio <luzzioj25@mail.wlu.edu>
2024-11-04 16:15:18 -0500

    Added in graph png and navbar menu's to organize site

--- ui.R
+++ ui.R
@@ -2,6 +2,7 @@
 
 library(shiny)
 library(shinythemes)
+
 navbarPage(theme = shinytheme("flatly"),
   "Concert Tickets USA",
            tabPanel("Welcome",
@@ -13,16 +14,23 @@ navbarPage(theme = shinytheme("flatly"),
                          However, concerts are pricey. Concert Tickets USA allows us to predict ticket prices based on a multitude of factors, and financially plan for that cost."),
                    br(), br(),
                    HTML("Julia Luzzio: Julia Luzzio is a biology major and a music minor. She is pre-veterinary and in her freetime enjoys reading, horseback riding, and coding!"),
-                   br(),
-                   HTML("Liz Blakely: Liz Blakely is a religion major..."),
-                   br(),
-                   HTML("Marcie Bernard: Marcie Bernard is a biology and environmental major..."),
-                   br(),
+                   br(), br(),
+                   HTML("Liz Blakely: Liz Blakely is a biology and religion major. She is pursuing a career in health policy. In her free time, she enjoys spending time with friend, reading, and going on hikes."),
+                   br(), br(),
+                   HTML("Marcie Bernard: Marcie Bernard is a biology and environmental major on the pre-veterinary track. In her free time, she walks dogs, spends time outdoors, and cooking."),
+                   br(), br(),
                    HTML("Zach Ricciardelli: Zach Ricciardelli is a...")),
-           tabPanel("Maps",
-                    h1("Ticket Price, Artist, and Venue Map Data")),
+           navbarMenu("Maps",
+                    tabPanel("City by City Ticket Price",
+                             h1("Ticket Price, Artist, and Venue Map Data"))),
            tabPanel("Graphical Cost Comparisons",
                     h1("Concert Ticket Graphical Cost Comparisons"),
-                    HTML("City by City Cost Context and Ticket Vendor Comparisons")),
-           tabPanel("Raw Data",
-                    h1("Raw Datasets for Ticket Data and Cost of Living")))
+                    HTML("Ticket Vendor Comparison Graph"),
+                    img(src = "Ticket_Vendor_Comparison.png")),
+           navbarMenu("Raw Data",
+                    tabPanel("Cumulative Ticket Data",
+                             h1("Cumulative Ticket Sale Data in the United States")),
+                    tabPanel("Cost of Living Data",
+                             h1("Raw Dataset of Cost of Living in United States Cities")),
+                    ))
+
