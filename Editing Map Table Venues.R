0b5bd8416341cf5a1a14dfdfe6321ed0cb66c678
Marcie Bernard <bernardm25@mail.wlu.edu>
2024-11-20 15:18:53 -0500

    trying to fix leaflet error with data set

---
+++ Editing Map Table Venues.R
@@ -0,0 +1,11 @@
+View(Updated_Venue_Avg_Price_Map_Data)
+Updated_Venue_Avg_Price_Map_Data$...1 <- NULL
+Updated_Venue_Avg_Price_Map_Data$Artist <- NULL
+Updated_Venue_Avg_Price_Map_Data$Venue.y <- NULL
+Updated_Venue_Avg_Price_Map_Data$City <- NULL
+Updated_Venue_Avg_Price_Map_Data$`Minimum Ticket Price` <- NULL
+Updated_Venue_Avg_Price_Map_Data$`Ticket Vendor` <- NULL
+Updated_Venue_Avg_Price_Map_Data$Month <- NULL
+Updated_Venue_Avg_Price_Map_Data$State <- NULL
+Updated_Venue_Avg_Price_Map_Data$`Cost of Living Index` <- NULL
+Updated_Venue_Avg_Price_Map_Data$Population <- NULL
