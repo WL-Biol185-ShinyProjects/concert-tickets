6b48ce1a391498937c24d3eacc84e89f8e3c650b
Zach Ricciardelli <ricciardelliz25@mail.wlu.edu>
2024-12-04 16:15:54 -0500

    changed lots of words, sizing, etc.

--- ui.R
+++ ui.R
@@ -18,12 +18,12 @@ navbarPage(theme = shinytheme("cerulean"),
                     mainPanel(h3("Concert Tickets USA is your virtual guide to concert tickets. Our site displays connections between ticket prices, artistis, locations, and more! Concerts are pricey. 
                        Concert Tickets USA shows you these prices, allowing you to to predict ticket prices in the future!"),
                               br(),
-                              h4("Our convenient features allow you to make the most educated decisions about your ticket purchase.
-                                 Use our artist search bar to view your favorite performer's past concerts and prices. View our interactive
-                                  maps to find optimum minimum ticket price by city, month, and venue. Our venur graph compares ticket vendors,
-                                 to inform you of the most cost efficient place to purchase your tickets from. Our interactive cost of living and minimum
-                                  ticket price graph help you view specific data about your home town. Finally, our entire dataset is at your 
-                                 fingertips to search with our convenient data search bar."),
+                              h4("Our features allow you to make the most educated decisions about your concert ticket purchases.
+                                 Use our artist search bar to view your favorite performer's past concerts and prices. View our interactive price
+                                  maps to reveal ticket price information by city, month, and venue. Our vendor graph compares ticket vendors
+                                 to help you choose where to buy your tickets. Our interactive cost of living and
+                                  ticket price graph allows you to view specific data about your hometown or other city. Finally, the dataset we used to build all of these features is at your 
+                                 fingertips to sift through and explore with a convenient search bar."),
                               br(),
                               br()),
                     img(src = "musicconcert.png")),
@@ -41,13 +41,12 @@ navbarPage(theme = shinytheme("cerulean"),
                    br(), br(),
                    h4("Julia Luzzio: Julia Luzzio is a biology major and a music minor. She is pre-veterinary and in her freetime enjoys reading, horseback riding, and coding!")),
                    
-                   img(src = "grouppic.png", width = "20%", height = "20%")),
+                   img(src = "grouppic.png", width = "50%", height = "50%")),
            tabPanel("Artist Search Bar",
                     fluidPage(
                       titlePanel("Artist Ticket Information"),
                       br(),
-                      h4("Search the name of your favorite performer and discover which cities they commonly perform at, what venues they attend in said city, 
-                         and the average minimum ticket price a consumer can find for one of their concerts."),
+                      h4("Search the name of your favorite performer and discover where they commonly perform as well as the average minimum ticket price you could find for one of their concerts."),
                       br(),
                       
                       sidebarLayout(
@@ -72,8 +71,8 @@ navbarPage(theme = shinytheme("cerulean"),
                     tabPanel("United States Average Ticket Price",
                              h1("United States Average Ticket Price by City (2016)"),
                              leafletOutput ("Average Ticket Price by City"),
-                             h4("The provided map allows the viewer to check the minimum average ticket price in a city of their choosing. Whether it is 
-                                your hometown or across the country, prospective ticket buyers can check where the most afforable concerts are being held.")
+                             h4("This map allows you to check the minimum average ticket price in any city! Whether it is 
+                                your hometown or all the way across the country, take a look at where the most afforable concerts are being held.")
                              ),
                     tabPanel("United States Monthly Average Ticket Price",
                             h1("United States Monthly Average Ticket Price by City (2016)"),
@@ -94,7 +93,10 @@ navbarPage(theme = shinytheme("cerulean"),
                                                       "November",
                                                       "December" ),
                                           selected = "January"),
-                              leafletOutput ("Average Ticket Price by City and Month"))),
+                              leafletOutput ("Average Ticket Price by City and Month"),
+                              h4("This map allows you to check the minimum average ticket price in any city during whichever month you select! Just choose a month and 
+                                 look at when and where the most afforable concerts are being held."))
+                              ),
                     tabPanel("Concert Ticket Prices of Big City Venues",
                              h1("Zoom in on any Major City to compare the concert ticket prices of different venues within each city!"), 
                              br(),
@@ -110,12 +112,11 @@ navbarPage(theme = shinytheme("cerulean"),
                     sidebarPanel(h3("Understand What You Are Paying For..."),
                                  br(),
                                  br(),
-                    HTML("This figure allows the buyer to understand which ticket vendor will
-                                      sell the most affordable concert tickets. Our data finds Wantickets to
-                                      be the most cost effective ticket vendor, and we recommend avoiding Live Nation.
-                                      While these are subject to change, the graphical representation should
-                                      help advise you to check as many vendors as possible and compare prices for
-                                      cost efficiency.
+                    HTML("This figure shows you which ticket vendor
+                                      typically sells the most affordable concert tickets. Our data finds Wantickets to
+                                      be the most cost effective ticket vendor, and Live Nation to be the most expensive.
+                                      While these are subject to change and we reccomend checking multiple websites before purchasing tickets, this graph 
+                                      can help to visualize the general trend.
                                       ")),            
                     img(src = "Ticket_Vendor_Comparison.png")),
             tabPanel("City Cost of Living vs. Minimum Ticket Price Graph",
@@ -127,7 +128,7 @@ navbarPage(theme = shinytheme("cerulean"),
                   mainPanel(h3("Travel to Get the Best Deal!"),
                             br(),
                   HTML("This graph outlines the comparison between the cost of living index and 
-                                 minimum average ticket price city by city. For your convenience, the 
+                                 minimum average ticket price for each city. For your convenience, the 
                                  graph is colored city by city, allowing visualization of the contrast between
                                  cities. With this tool, you can find cities with the most affordable ticket 
                                  prices. Hold and brush over the graph to compare other factors of each data point,
